9d64dfe736cb6c678932de039a303e669ac397f9
Elena Surilova <elen21surilova@gmail.com>
2023-04-03 18:47:47 +0300

    feat: create a primary structure of components
    create a primary structure according to the drawn scheme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ReadbooksListComponent } from './readbooks-list/readbooks-list.component';
+import { SearchBooksComponent } from './books-list/search-books/search-books.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { EditProfileComponent } from './user-profile/edit-profile/edit-profile.component';
+import { UserSigninComponent } from './user-signin/user-signin.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksListComponent,
+    WishlistComponent,
+    ReadbooksListComponent,
+    SearchBooksComponent,
+    BookDetailsComponent,
+    UserProfileComponent,
+    EditProfileComponent,
+    UserSigninComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/readbooks-list/readbooks-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-readbooks-list',
+  templateUrl: './readbooks-list.component.html',
+  styleUrls: ['./readbooks-list.component.scss']
+})
+export class ReadbooksListComponent {
+
+}
