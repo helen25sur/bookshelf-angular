9d7e8eac9195cf541f44040559584470c1c6fbc9
Elena Surilova <elen21surilova@gmail.com>
2023-04-06 01:59:21 +0300

    fix: change books list component
    change this component with Angular Material Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksListComponent } from './books-list/books-list.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ReadbooksListComponent } from './readbooks-list/readbooks-list.component';
+import { UserSigninComponent } from './user-signin/user-signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: BooksListComponent,
+  },
+  {
+    path: 'wishlist', component: WishlistComponent,
+  },
+  {
+    path: 'readbooks', component: ReadbooksListComponent,
+  },
+  {
+    path: 'sign-in', component: UserSigninComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 // Angular Material
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
 
 const MaterialComponents = [
+  MatSidenavModule,
+  MatToolbarModule,
   MatButtonModule,
+  MatListModule,
+  MatIconModule,
+  MatMenuModule,
+  MatCardModule,
 ]
 
 @NgModule({
