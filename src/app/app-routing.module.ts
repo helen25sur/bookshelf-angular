943635ac7ceecb9930e54dd4eb8bded837d19fcf
Elena Surilova <elen21surilova@gmail.com>
2023-04-11 17:47:12 +0300

    fix: delete several components
    delete additional components - readbooks-list, wishlist and user-components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksListComponent } from './books-list/books-list.component';
-import { WishlistComponent } from './wishlist/wishlist.component';
-import { ReadbooksListComponent } from './readbooks-list/readbooks-list.component';
-import { UserSigninComponent } from './user-signin/user-signin.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 const routes: Routes = [
   {
     path: '', component: BooksListComponent,
   },
   {
-    path: 'wishlist', component: WishlistComponent,
+    path: 'books/:bookId', component: BookDetailsComponent,
   },
-  {
-    path: 'readbooks', component: ReadbooksListComponent,
-  },
-  {
-    path: 'sign-in', component: UserSigninComponent,
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksListComponent } from './books-list/books-list.component';
-import { WishlistComponent } from './wishlist/wishlist.component';
-import { ReadbooksListComponent } from './readbooks-list/readbooks-list.component';
 import { SearchBooksComponent } from './books-list/search-books/search-books.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { EditProfileComponent } from './user-profile/edit-profile/edit-profile.component';
-import { UserSigninComponent } from './user-signin/user-signin.component';
-import { UserLoginComponent } from './user-login/user-login.component';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksListComponent,
-    WishlistComponent,
-    ReadbooksListComponent,
     SearchBooksComponent,
     BookDetailsComponent,
-    UserProfileComponent,
-    EditProfileComponent,
-    UserSigninComponent,
-    UserLoginComponent
   ],
   imports: [
     BrowserModule,
